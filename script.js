76409454efc2a08ae88eb2827501c8eb0041d31e
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2026-04-28 18:47:33 -0400

    feat: build the api url and fetch articles

---
+++ script.js
@@ -0,0 +1,14 @@
+const API_KEY = "9ae5290a-938e-46bc-885a-8e24e19783e8";
+
+function getNews() {
+    const url = `https://content.guardianapis.com/search?api-key=${API_KEY}&show-fields=thumbnail,trailText&page-size=20`;
+
+    fetch(url).then(response => response.json()).then(data => {
+        const articles = data.response.results;
+        console.log(articles);
+
+    }).catch(error => {
+        console.error("Error fetching news:", error); 
+
+    });
+}

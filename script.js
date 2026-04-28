fa2db6e1a97ee4616d005a9ba8580dc5176d3498
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2026-04-28 19:02:41 -0400

    feat: add articles array and favorites dictionary

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const API_KEY = "9ae5290a-938e-46bc-885a-8e24e19783e8";
 
+articles = []
+favorites = {}
+
 function getNews() {
     const url = `https://content.guardianapis.com/search?api-key=${API_KEY}&show-fields=thumbnail,trailText&page-size=20`;
 
     fetch(url).then(response => response.json()).then(data => {
-        const articles = data.response.results;
+        articles = data.response.results;
         console.log(articles);
 
     }).catch(error => {

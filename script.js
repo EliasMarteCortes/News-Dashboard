c28611e8aeb456a1367e0097a9c67932a2e9aa69
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2026-04-28 20:03:49 -0400

    feat: render articles as cards on the page

--- script.js
+++ script.js
@@ -17,9 +17,39 @@ function getNews(category, query) {
     fetch(url).then(response => response.json()).then(data => {
         articles = data.response.results;
         console.log(articles);
-
+        showNews(articles, "news-container");
     }).catch(error => {
         console.error("Error fetching news:", error); 
 
     });
 }
+
+function showNews(list, containerId) {
+    let newsContainer = document.getElementById(containerId);
+    newsContainer.innerHTML = "";
+
+    if (list.length === 0) {
+        newsContainer.innerHTML = "<p>No news found.</p>";
+        return;
+    }
+
+    for (let i = 0; i < list.length; i++) {
+        let article = list[i];
+        let thumbnail = article.fields.thumbnail ? article.fields.thumbnail: "https://placehold.co/280x160?text=No+Imag ";
+        let desc = article.fields.trailText ? article.fields.trailText : "No description available.";
+
+        let card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+            <img src="${thumbnail}" alt="Article Image">
+            <h3>${article.webTitle}</h3>
+            <p>${desc}</p>
+            <a href="${article.webUrl}" target="_blank">Read more</a>
+        `;
+
+        newsContainer.appendChild(card);
+    }
+};
+
+getNews();
